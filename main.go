c99fda40d872d96384e3fab3c3239716a4174a1a
sheepla <62412884+sheepla@users.noreply.github.com>
2022-03-05 17:58:38 +0900

    Implement help message on interactive mode

--- main.go
+++ main.go
@@ -18,6 +18,36 @@ const (
 	appVersion = "0.0.1"
 )
 
+const helpMessage = `
+COMMANDS
+    exit  Quit interactive UI
+    help  Show help message
+
+KEY BINDINGS
+  Ctrl-A, Home          Move cursor to beginning of line
+  Ctrl-E, End           Move cursor to end of line
+  Ctrl-B, Left          Move cursor one character left
+  Ctrl-F, Right         Move cursor one character right
+  Ctrl-Left, Alt-B      Move cursor to previous word
+  Ctrl-Right, Alt-F     Move cursor to next word
+  Ctrl-D, Del           (if line is not empty) Delete character under cursor
+  Ctrl-D                (if line is empty) End of File - usually quits application
+  Ctrl-C                Reset input (create new empty prompt)
+  Ctrl-L                Clear screen (line is unmodified)
+  Ctrl-T                Transpose previous character with current character
+  Ctrl-H, BackSpace     Delete character before cursor
+  Ctrl-W, Alt-BackSpace Delete word leading up to cursor
+  Alt-D                 Delete word following cursor
+  Ctrl-K                Delete from cursor to end of line
+  Ctrl-U                Delete from start of line to cursor
+  Ctrl-P, Up            Previous match from history
+  Ctrl-N, Down          Next match from history
+  Ctrl-R                Reverse Search history (Ctrl-S forward, Ctrl-G cancel)
+  Ctrl-Y                Paste from Yank buffer (Alt-Y to paste next yank instead)
+  Tab                   Next completion
+  Shift-Tab             (after Tab) Previous completion
+`
+
 type exitCode int
 
 const (
@@ -98,6 +128,11 @@ func repl() exitCode {
 			continue
 		}
 
+		if code == "help" {
+			fmt.Print(helpMessage)
+			continue
+		}
+
 		if code == "exit" {
 			break
 		}
@@ -107,6 +142,7 @@ func repl() exitCode {
 		}
 		result, err := client.Post(p)
 		if err != nil {
+			log.Println(err)
 			continue
 		}
 		if result.Stdout != "" {
