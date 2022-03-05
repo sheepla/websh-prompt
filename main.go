5e9421bd59998012231cf1de554e5c89fe77f09a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-03-05 12:27:28 +0900

    Windows colors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/jessevdk/go-flags"
+	"github.com/mattn/go-colorable"
 	"github.com/peterh/liner"
 	"github.com/sheepla/websh-prompt/client"
 )
@@ -25,6 +26,11 @@ const (
 type options struct {
 	Version bool `short:"V" long:"version" description:"Show version"`
 }
+
+var (
+	stdout = colorable.NewColorableStdout()
+	stderr = colorable.NewColorableStderr()
+)
 
 func main() {
 	os.Exit(int(Main(os.Args[1:])))
@@ -77,10 +83,10 @@ func Main(args []string) exitCode {
 			continue
 		}
 		if result.Stdout != "" {
-			fmt.Fprintln(os.Stdout, result.Stdout)
+			fmt.Fprintln(stdout, result.Stdout)
 		}
 		if result.Stderr != "" {
-			fmt.Fprintln(os.Stderr, result.Stderr)
+			fmt.Fprintln(stderr, result.Stderr)
 		}
 	}
 }
